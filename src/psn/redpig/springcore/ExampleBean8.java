a7239513e44d8602b955fa5a99193bcc7f988b5d
redpig <lihuang_man@hotmail.com>
2019-01-22 23:18:10 +0800

    通过实现spring的InitializingBean和DisposableBean接口进行初始化和销毁回调

--- src/psn/redpig/springcore/ExampleBean8.java
+++ src/psn/redpig/springcore/ExampleBean8.java
@@ -3,6 +3,11 @@ package psn.redpig.springcore;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+/**
+ * ExampleBean8
+ * @author john
+ * 通过实现spring的InitializingBean和DisposableBean接口进行初始化和销毁回调
+ */
 public class ExampleBean8 implements InitializingBean, DisposableBean {
 
 	@Override
